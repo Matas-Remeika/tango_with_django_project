bf2e4b371ea71e6f8edf963f3f71dc2b43db157e
Matas Remeika <2526663r@student.gla.ac.uk>
2022-02-11 17:39:38 +0000

    Chapter 10 tests

--- rango/views.py
+++ rango/views.py
@@ -18,13 +18,17 @@ def index(request):
 
 	visitor_cookie_handler(request)
 
-	context_dict['visits'] = request.session['visits']
-
 	response = render(request, 'rango/index.html', context=context_dict)
 	return response
 
 def about(request):
-	return render(request, 'rango/about.html', context={})
+	context_dict = {}
+
+	visitor_cookie_handler(request)
+
+	context_dict['visits'] = request.session['visits']
+
+	return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
 	context_dict = {}
