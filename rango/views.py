fad559ac8466d8fff011cc897bf3944317e70056
Matas Remeika <2526663r@student.gla.ac.uk>
2022-02-11 16:15:09 +0000

    Chapter 9 tests

--- rango/views.py
+++ rango/views.py
@@ -35,6 +35,7 @@ def show_category(request, category_name_slug):
 
 	return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
 	form = CategoryForm()
 
@@ -49,6 +50,7 @@ def add_category(request):
 
 	return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
 	try:
 		category = Category.objects.get(slug=category_name_slug)
@@ -136,4 +138,4 @@ def user_logout(request):
 
 @login_required
 def restricted(request):
-	return HttpResponse("Since you're logged in, you can see this text!")
+	return render(request, 'rango/restricted.html')
